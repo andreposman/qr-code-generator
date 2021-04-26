6dca99a12a6b4f276ff1ff7e6d24f77be5ca56ec
André de Fazzio Posman <andre.posman@gmail.com>
2021-04-26 16:04:57 -0300

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/andreposman/qr-code-generator"
+	qrcode "github.com/andreposman/qr-code-generator"
 )
 
 func main() {
@@ -11,7 +11,7 @@ func main() {
 	fileName := "qrcode.png"
 
 	QRCode := qrcode.Generate("123-456")
-	qr.SaveQRCode(fileName, QRCode)
+	qrcode.SaveQRCode(fileName, QRCode)
 }
 
 func greeting() {
